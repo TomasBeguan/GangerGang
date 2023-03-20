9044b7dfb0a9f983060b33203e5311cd5548b2a0
TomasBeguan <tomasbeguan@gmail.com>
2023-03-20 01:46:29 -0300

    v-0.2 agregado text to speech y otras funcionalidades

--- script.js
+++ script.js
@@ -27,19 +27,30 @@ $(document).ready(function(){
    
   const name_card = document.getElementById("name_card");
   const desc_card = document.getElementById("desc_card");
+  const dialogue_card = document.getElementById("dialogue_card");
   const nacionalidad_card = document.getElementById("nacionalidad_card");
   const imageUrl_card = document.getElementById("imageUrl_card");
   const gender_card = document.getElementById("gender_card");
 
-  function generar(){
-    playSound();
-    rotarCard();
+  let index = null;
+  let dialogue = null;
 
-      const index = Math.floor(Math.random() * gangerArray.length); // Generamos un índice aleatorio
-      console.log(index)
+  let dialogue_state = false;
+  let dialogue_index = 0;
+  
+  function generar(valor){
+
+    console.log(dialogue_index)
+
+    if (valor == 'generar'){
+      playSound();
+      rotarCard();
+
+      index = Math.floor(Math.random() * gangerArray.length); // Generamos un índice aleatorio
 
       const nombre = gangerArray[index].name; // Obtenemos el nombre del personaje seleccionado
       const descripcion = gangerArray[index].desc; // Obtenemos la descripción del personaje seleccionado
+      dialogue = gangerArray[index].dialogue;
       const imageUrl = gangerArray[index].imageUrl;
       const urlImagen = gangerArray[index].icono_url; // Obtenemos la URL de la imagen correspondiente al personaje seleccionado
       const nacionalidad_title = gangerArray[index].nacionalidad
@@ -48,15 +59,56 @@ $(document).ready(function(){
       const imagePreload = document.getElementById("imagePreload"); // Obtenemos la imagen pre-cargada
       imagePreload.setAttribute("src", imageUrl);
 
+      if(dialogue_state == true){
+        const dialogue_card = document.getElementById("dialogue_card");
+        dialogue_card.classList.toggle(`dialogue_cont_open`)
+        dialogue_state = false
+      }
+
+
     setTimeout(function() {
       name_card.innerHTML = nombre; // Asignamos el nombre al elemento HTML correspondiente
       desc_card.innerHTML = descripcion; // Asignamos la descripción al elemento HTML correspondiente
+      dialogue_card.innerHTML = dialogue[dialogue_index];
       imageUrl_card.style.backgroundImage = `url(${imageUrl})`;
       nacionalidad_card.style.backgroundImage = `url(${urlImagen})`;
       nacionalidad_card.title = nacionalidad_title
       gender_card.style.backgroundImage = `url("assets/${gender}.png")`;
       
     }, 500);
+    }
+
+
+    if (valor == 'dialogue') {
+      const dialogue_card = document.getElementById("dialogue_card");
+      var msg = new SpeechSynthesisUtterance();
+    
+      if (!dialogue_state) {
+        dialogue_card.classList.toggle(`dialogue_cont_open`);
+        dialogue_state = true;
+
+        msg.text = dialogue[dialogue_index];
+        window.speechSynthesis.speak(msg);
+        
+
+      } else {
+        if(dialogue_index < dialogue.length-1){
+          dialogue_index++;
+        }else{
+          dialogue_index = 0
+        }
+        dialogue_card.classList.toggle(`dialogue_cont_open`);
+
+        msg.text = dialogue[dialogue_index];
+        window.speechSynthesis.speak(msg);
+
+        setTimeout(function() {
+          dialogue_card.innerHTML = dialogue[dialogue_index];
+          dialogue_card.classList.toggle(`dialogue_cont_open`);
+        }, 300);
+      }
+    }
+    
    
   }
 
@@ -71,6 +123,9 @@ $(document).ready(function(){
       image_preload_div.appendChild(nuevoDiv);
     }
   }
+
+
+
 
 
   //ROTAR
